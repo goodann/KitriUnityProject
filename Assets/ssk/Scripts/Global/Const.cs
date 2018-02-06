44b6dd98b5d46b713af7d74b0dcf3ce6cc0deb79
jowansik <jowansik@naver.com>
2018-02-06 15:20:02 +0900

    global 수정

--- Assets/ssk/Scripts/Global/Const.cs
+++ Assets/ssk/Scripts/Global/Const.cs
@@ -7,5 +7,7 @@ public static class Const{
 
     //캐릭터 상태
  
-    
+    public const string Enemy_Melee = "Enemy_Melee";
+    public const string Enemy_Archor = "Enemy_Archor";
+    public const string Enemy_Boss = "Enemy_Boss";
 }
