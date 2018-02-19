0da41cb63b03cfa456fa1d23097552e24ff8c1ec
gakki0225 <gakki0225@naver.com>
2018-02-19 13:55:59 +0900

    ed

---
+++ Assets/kym/MAP01/02.Scripts/LobbyStageBtn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyStageBtn : MonoBehaviour {
+
+    public void OnClickStageBtn()
+    {
+        SceneManager.LoadScene("STAGE", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/kym/MAP01/02.Scripts/Stage_BUILDING_Btn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage_BUILDING_Btn : MonoBehaviour {
+
+    public void OnClick_BUILDING_Btn()
+    {
+        SceneManager.LoadScene("MAP_MODERN_BUILDING", LoadSceneMode.Single);
+    }
+}
