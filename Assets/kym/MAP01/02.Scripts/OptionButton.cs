8a276fdf2ab043c1b2c10f27d579e41b00978729
gakki0225 <gakki0225@naver.com>
2018-02-23 14:26:14 +0900

    ddd

---
+++ Assets/kym/MAP01/02.Scripts/OptionButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionButton : MonoBehaviour {
+
+    public GameObject optionPanel = null;
+
+    public void OnClickOptionBtn()
+    {
+        optionPanel.SetActive(true);
+    }
+}
---
+++ Assets/kym/MAP01/02.Scripts/UIComboControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIComboControl : MonoBehaviour {
+
+    Text comboText;
+    Animator comboAnim;
+    [SerializeField]
+    int comboCnt = 0;
+
+    private void Start()
+    {
+        comboText = GetComponent<Text>();
+        comboAnim = GetComponent<Animator>();
+    }
+
+    public void SetComboOnAnim()
+    {
+        comboAnim.SetBool("ComboOn", true);
+    }
+
+    public void SetComboOffAnim()
+    {
+        if(comboAnim != null)
+        {
+            comboAnim.SetBool("ComboOn", false);
+            
+        }
+            
+    }
+
+
+    public void SetCombo(int cnt)
+    {
+        if(comboAnim != null)
+        {
+            comboCnt = cnt;
+            comboText.text = comboCnt + "COMBO!!";
+        }
+    }
+}
